189e15195661eefe80a3d9c8638f2fff8c2888f8
jeffrey <jeffrey.australie@gmail.com>
2019-06-19 14:27:52 +0200

    intall bramus

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php 
-
+use Bramus\Router\Router;
 require __DIR__ . '/vendor/autoload.php';
- $article=[
-     "1",
-     "2",
-     "3",
 
- ];
+$router =new Router;
 
- var_dump($article);
- dump($article);
+$router->get('/hello', function(){
+    echo "hello world";
+});
+
+$router->get('/articles', function () {
+    echo "liste des articles";
+});
+
+$router->get('/articles/{id}', function ($id) {
+    echo "voici l'article " . $id;
+});
+
+$router->run();
